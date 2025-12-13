6aa2e195ba0883f6a956381a31914d65d4da6113
JejeNup <jesayaphs24@gmail.com>
2025-12-13 10:51:58 +0700

    panahh

--- Main.cpp
+++ Main.cpp
@@ -180,3 +180,31 @@ int Loadingy = max_y/2;
         mvprintw(max_y-2, 2, " ||[X]|_|");    
         attroff(COLOR_PAIR(3));
         
+        switch (panah)
+        {
+        case KEY_UP:
+            if (y > 0){
+                score = score + 10;
+            }
+            y = (y > 0) ? y - 1 : y; 
+    
+            break;                   
+
+        case KEY_DOWN:
+            if (y < LINES - 1){
+
+            score = score - 10;
+            }
+            y = (y < LINES - 1) ? y + 1 : y; 
+
+            break;                          
+
+        case KEY_LEFT:
+            x = (x > 0) ? x - 1 : x; 
+            break;                   
+
+        case KEY_RIGHT:
+            x = (x < COLS - 1) ? x + 1 : x; 
+            break;                          
+
+        }
